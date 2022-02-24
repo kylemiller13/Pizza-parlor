09dd93f8c46539cbfee5df249bc76b562d22f225
Kyle Miller <millerkyle85@gmail.com>
2022-02-24 14:16:40 -0800

    start business logic

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+//Business Logic 
+function Pizza(toppings, size){
+  this.toppings = toppings;
+  this.size = size;
+  this.totalCost = 0;
+}
+
+Pizza.prototype.price = function(){
+  let prize = 
+}
+
