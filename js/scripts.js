d036c1bf7976373cf86c19c66fe20018204d0eb4
Kyle Miller <millerkyle85@gmail.com>
2022-02-24 23:12:51 -0800

    work on UI Logic

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,32 @@
 function Pizza(toppings, size){
   this.toppings = toppings;
   this.size = size;
-  this.totalCost = 0;
+ 
 }
+// let newPizza = new Pizza();
 
 Pizza.prototype.price = function(){
-  let prize = 
+  let price = 14;
+  for (let i = 0; i < this.toppings.length; i++){
+    if (this.toppings[i] === "pepperroni" || "sausage" || "onions"){
+      price += 1;
+    }
+  }
+  if (this.size === "medium"){
+    price += 3;
+  }
+  if (this.size === "large"){
+    price += 3;
+  }
+  return price;
 }
 
+//UI Logic
+$(document).ready(function() {
+  $("#pizzaOrder").submit(function(event){
+    let size = $('#size option:selected').text();
+    let toppings = $('input[name=topping]:checked').val();
+
+    event.preventDefault();
+  });
+});
